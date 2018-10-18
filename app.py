e1840cb159957a7f1073e752c90c9d442bcfdecd
Mike Toriello <34101012+mtoriello0725@users.noreply.github.com>
2018-10-18 13:34:30 -0400

    Updates the remaining API routes. Provided example text for user input for start and end dates

--- app.py
+++ app.py
@@ -44,14 +44,35 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/><br/>"
+        f"Example of start and end API call:<br/>"
+        f"/api/v1.0/2016-10-02/2016-10-08"
     )
 
+""" Question on REPO asks for dictionary with date as keys, and tobs as value
+	This is impossible because there are multiple stations, therefore dates will
+	overlap. Dates must be unique in order to make them keys. Also without the station 
+	ID, this data does not provide any significant insight. """
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+	""" Return a list of all precipitation values by station ordered by date"""
+	results = session.query(measurement.date, measurement.station, measurement.prcp).\
+		order_by(measurement.date).all()
 
+	all_prcp = []
+	for precip in results:
+		prcp_dict = {}
+		prcp_dict["date"] = precip.date
+		prcp_dict["station"] = precip.station
+		prcp_dict["prcp"] = precip.prcp
+		all_prcp.append(prcp_dict)
+
+	return jsonify(all_prcp)
+
+""" Returns list of all the stations """
 @app.route("/api/v1.0/stations")
 def stations():
-    """Return a list of all stations"""
     # Query all stations
     results = session.query(measurement.station).\
     	group_by(measurement.station).all()
@@ -61,9 +82,9 @@ def stations():
 
     return jsonify(all_stations)
 
+""" Returns list of all temperatures for the last year """
 @app.route("/api/v1.0/tobs")
 def tobs():
-	""" Return a list of all temperatures for the last year"""
 	# Query all measurements
 	sel = [measurement.station, measurement.tobs, measurement.date]
 	results = session.query(*sel).\
@@ -79,6 +100,23 @@ def tobs():
 		all_temps.append(temp_dict)
 
 	return jsonify(all_temps)
+
+""" Returns a list of the lowest, average, and highest temp since a specified start date """
+@app.route("/api/v1.0/<start>")
+def temp_start(start):
+	# start should be in format (yyyy-mm-dd)
+	results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+		filter(measurement.date >= start).all()
+
+	return jsonify(results)
+""" Returns a list of the lowest, average, and highest temp since a specified start and end date """
+@app.route("/api/v1.0/<start>/<end>")
+def temp_start_end(start,end):
+	# start should be in format (yyyy-mm-dd)
+	results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+		filter(measurement.date >= start).filter(measurement.date <= end).all()
+
+	return jsonify(results)
 
 
 if __name__ == '__main__':
